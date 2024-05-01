b302dd935fe62d14ca5530b8bbb6008af7437d88
SaadNasir92 <nas.saad53@gmail.com>
2024-05-01 18:46:14 -0500

    Completed scatter function, next cycle working on linear regression function.

--- util/functions.py
+++ util/functions.py
@@ -2,8 +2,15 @@ import matplotlib.pyplot as plt
 from scipy.stats import linregress
 import pandas as pd
 
-def make_scatter(df, x, y):
-    return df.plot.scatter(x,y, edgecolors='black', alpha = .75)
+def make_scatter(df, x, y, x_label, y_label, title, **kwargs):
+    fig, ax = plt.subplots()
+    ax.grid(True)
+    ax.scatter(df[x],df[y], **kwargs)
+    ax.set_xlabel(x_label)
+    ax.set_ylabel(y_label)
+    ax.set_title(title)
+    
+    return fig, ax
 
 def make_linregress(df, m, b, x, y, y_pred):
     pass
